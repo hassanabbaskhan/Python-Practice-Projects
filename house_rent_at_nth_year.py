a2d72baf64477a5264347086457771eec3102fe6
HASSAN ABBAS KHAN <97775962+hassanabbaskhan@users.noreply.github.com>
2022-10-01 23:33:50 +0500

    Update house_rent_at_nth_year.py

--- house_rent_at_nth_year.py
+++ house_rent_at_nth_year.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
 #this code is to calculate the house rent after nth no of year.
 #assuming the increase in rent remains the same, like 10 percent annually.
 #now let's start typing the code
@@ -12,10 +6,3 @@ x=int(input("Enter the current rent \n"))
 y=int(input("Enter the no of years, after which the rent to be calculated \n"))
 rent=x*1.1**y #1.1 is actually 10% increase of rent annually, which would be written as (100% + 10%)/100
 print("The rent after",y,"years will be",rent)
-
-
-# In[ ]:
-
-
-
-
