173ef7ebfa6b427635e2163bd980a76c653d10dd
HASSAN ABBAS KHAN <97775962+hassanabbaskhan@users.noreply.github.com>
2022-10-02 00:09:43 +0500

    Add files via upload

---
+++ input function and Lists (user name and password checking).py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[26]:
+
+
+#make a form and ask the user to put username and password correctly so that the next procedure may be followed ahead.
+#taking user inputs of username and password
+
+username=input("Enter user name \n")
+password=input("Enter password \n")
+data=["hassan","abc123"]     #stored_user_info_name_password
+if username==data[0] and password==data[1]:
+    print("Qualified for next procedure")
+elif username!=data[0] and password==data[1]:
+    print("incorrect username, re-try")
+elif username==data[0] and password !=data[1]:
+    print("Forget password?")
+else:
+    print("username and password are incorrect")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
