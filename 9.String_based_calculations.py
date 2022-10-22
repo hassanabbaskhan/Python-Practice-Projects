710c39de1d6108ac16ea1d81c6ca4b0f39071506
HASSAN ABBAS KHAN <97775962+hassanabbaskhan@users.noreply.github.com>
2022-10-22 13:58:35 +0500

    Create 9.String_based_calculations.py
    The code is to count the no of repetition of letters, strings, etc

---
+++ 9.String_based_calculations.py
@@ -0,0 +1,13 @@
+"""
+Some string based calculations and comprehension* - 
+count repetition of letters or words.
+
+string= we are learning.
+check repetition of = ar
+
+"""
+
+str = input("Enter the string \n")
+x=input("enter to count the repitition of \n")
+y=str.count(x)
+print (x," is repeated  ",y," times")
