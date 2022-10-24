76b0b27497ac01875cbf4d6d212c494c5b3a8f45
HASSAN ABBAS KHAN <97775962+hassanabbaskhan@users.noreply.github.com>
2022-10-24 13:53:36 +0500

    Add files via upload

---
+++ a11_Separate_list_format.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Q 5
+# Take 3 seperate List of 
+#     Names
+#     age
+#     birth date
+# 
+# and combine them and print them as single list like
+# ('ali', 33, '1985_10_1')
+# ('hassan', 30, '1990_12_1)
+
+# In[15]:
+
+
+name=['ali','hassan','akbar']
+age=[30,40,20]
+birth_date=['1992_12_30','1982_05_13','2002_04_22']
+list1=name[0],age[0],birth_date[0]
+list2=name[1],age[1],birth_date[1]
+list3=name[2],age[2],birth_date[2]
+print(list1)
+print(list2)
+print(list3)
+
+
+# In[ ]:
+
+
+
+
